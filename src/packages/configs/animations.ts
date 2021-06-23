e91757c292aa0edee0d577a90c0a8822c6090b3f
Vladimir Migay <vova.migay123@gmail.com>
2021-06-23 18:07:26 +0300

    add animations

---
+++ src/packages/configs/animations.ts
@@ -0,0 +1,50 @@
+import { keyframes } from 'styled-components';
+
+export const rotate = keyframes`
+  0% {
+    transform: rotate(0deg) translate3d(0, 0, 0);
+  }
+  25% {
+    transform: rotate(3deg) translate3d(0, 0, 0);
+  }
+  50% {
+    transform: rotate(-3deg) translate3d(0, 0, 0);
+  }
+  75% {
+    transform: rotate(1deg) translate3d(0, 0, 0);
+  }
+  100% {
+    transform: rotate(0deg) translate3d(0, 0, 0);
+  }
+`;
+
+export const appearSide = keyframes`
+  0% {
+    overflow: hidden;
+    width: 0px;
+    opacity: 0;
+  }
+  100% {
+    overflow: hidden;
+    width: 100px;
+    opacity: 1;
+  }
+`;
+
+export const storm = keyframes`
+  0% {
+    transform: translate3d(0, 0, 0) translateZ(0);
+  }
+  25% {
+    transform: translate3d(4px, 0, 0) translateZ(0);
+  }
+  50% {
+    transform: translate3d(-3px, 0, 0) translateZ(0);
+  }
+  75% {
+    transform: translate3d(2px, 0, 0) translateZ(0);
+  }
+  100% {
+    transform: translate3d(0, 0, 0) translateZ(0);
+  }
+`;
