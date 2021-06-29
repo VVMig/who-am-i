5b17a363657f39847cbbbea1ef0d620aa568c919
Vladimir Migay <vova.migay123@gmail.com>
2021-06-29 14:16:04 +0300

    Add switch function

--- src/app/layout/header/SwitchLanguage.tsx
+++ src/app/layout/header/SwitchLanguage.tsx
@@ -1,32 +1,44 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
-import { Dropdown } from '../../../packages';
-import { useOutsideClick } from '../../hooks';
+import { Dropdown, DropdownItem } from '../../../packages';
+import { useCustomTranlation, useOutsideClick } from '../../hooks';
+import { languages } from '../../languages';
 import { Styled } from './styled';
 
 interface Props {
   isLanguageActive: boolean;
-  onToggleLang: () => void;
+  onToggleLanguage: () => void;
 }
 
 export const SwitchLanguage: React.FC<Props> = ({
   isLanguageActive,
-  onToggleLang,
+  onToggleLanguage,
 }) => {
   const switchLanguageRef = useRef<HTMLDivElement | null>(null);
+  const { changeLanguage, i18n } = useCustomTranlation();
+
+  const [translations] = useState<DropdownItem[]>(
+    languages.map((language) => ({
+      text: language,
+      onClickHandler: () => {
+        onToggleLanguage();
+        changeLanguage(language);
+      },
+    }))
+  );
 
   useOutsideClick(switchLanguageRef, () => {
     if (isLanguageActive) {
-      onToggleLang();
+      onToggleLanguage();
     }
   });
 
   return (
     <Styled.SwitchLangWrapper ref={switchLanguageRef}>
-      <Styled.SwitchLang isActive={isLanguageActive} onClick={onToggleLang}>
-        En
+      <Styled.SwitchLang isActive={isLanguageActive} onClick={onToggleLanguage}>
+        {i18n.language}
       </Styled.SwitchLang>
-      <Dropdown isShow={isLanguageActive} dropdownItems={[]} />
+      <Dropdown isShow={isLanguageActive} dropdownItems={translations} />
     </Styled.SwitchLangWrapper>
   );
 };
