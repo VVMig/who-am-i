dc0188f3149a2d6f1c54cc5187f6a6d4c868b2d3
Vladimir Migay <vova.migay123@gmail.com>
2021-07-02 16:01:40 +0300

    Add leave and reconnect functions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useMutation, useQuery } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 
+import { ConfirmModal } from '../packages';
 import { ErrorAlert } from './error-alert';
+import { useCustomTranslation } from './hooks';
 import { Header } from './layout';
 import {
   IsRoomExistQuery,
   IS_ROOM_EXIST,
+  LEAVE_ROOM,
   ReconnectRoomQuery,
   RECONNECT_ROOM,
 } from './query';
@@ -16,15 +19,29 @@ import { RoutesSwitch } from './RoutesSwitch';
 import { store } from './store';
 
 export const App = () => {
+  const { t } = useCustomTranslation();
+
+  const [isModalShow, setIsModalShow] = useState(false);
   const history = useHistory();
 
   const [reconnectRoom] = useMutation<ReconnectRoomQuery>(RECONNECT_ROOM);
+  const [leaveRoom] = useMutation(LEAVE_ROOM);
   const { data, loading, error } = useQuery<IsRoomExistQuery>(IS_ROOM_EXIST);
+
+  const leave = async () => {
+    try {
+      await leaveRoom();
+      setIsModalShow(false);
+    } catch (error) {
+      store.error.setError(error.message);
+    }
+  };
 
   const reconnect = async () => {
     try {
       const { data } = await reconnectRoom();
 
+      setIsModalShow(false);
       if (data?.reconnectRoom) {
         history.push(`${RoutesEnum.Game}?id=${data.reconnectRoom.shareId}`);
       }
@@ -39,12 +56,20 @@ export const App = () => {
     }
 
     if (data?.isRoomExist && !loading) {
-      reconnect();
+      setIsModalShow(true);
     }
   }, [data, error]);
 
   return (
     <>
+      {isModalShow && (
+        <ConfirmModal
+          text={t('app.reconnectConfirm')}
+          onClickDenied={leave}
+          onClickConfirm={reconnect}
+          handleModalClose={leave}
+        />
+      )}
       <ErrorAlert />
       <Header />
       <RoutesSwitch />
