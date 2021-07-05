62946f9e7a78cda1a86362c6605e6e726bb6c214
Vladimir Migay <vova.migay123@gmail.com>
2021-07-05 16:22:55 +0300

    Add wslink

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { App } from './app/App';
+import { client } from './client';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme';
-import { serverUri } from './uri';
 
 import './i18n';
-
-const link = createHttpLink({
-  uri: serverUri,
-  credentials: 'include',
-});
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link,
-});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
--- src/uri.ts
+++ src/uri.ts
@@ -1,1 +1,3 @@
 export const serverUri = 'http://localhost:4000/graphql';
+
+export const wsUri = 'ws://localhost:4000/subscriptions';
