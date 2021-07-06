573c9fc14633e6ca2e64fb69fd21a7e4afd1c54d
Vladimir Migay <vova.migay123@gmail.com>
2021-07-06 16:26:04 +0300

    Add gameUser to root state

--- src/app/store/rootStore.ts
+++ src/app/store/rootStore.ts
@@ -1,6 +1,8 @@
+import { cloneDeep } from 'lodash';
 import { cast, types } from 'mobx-state-tree';
 
 import { Error } from './error';
+import { GameUser } from './GameUser';
 import { IRoomTypes } from './interfaces';
 import { Room } from './Room';
 
@@ -8,10 +10,20 @@ export const RootStore = types
   .model({
     error: Error,
     room: types.optional(Room, {}),
+    gameUser: types.maybe(GameUser),
   })
   .actions((self) => ({
     setRoom(room: IRoomTypes) {
       self.room = cast(room);
+    },
+    setGameUser(id: string) {
+      const gameUser = self.room.participants.find(
+        (participant) => participant.id === id
+      );
+
+      if (gameUser) {
+        self.gameUser = cast(cloneDeep(gameUser));
+      }
     },
   }))
   .views((self) => ({
