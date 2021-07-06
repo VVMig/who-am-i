fdd478eedc5dfb701d1105fd59b2b0b48b64eb46
Vladimir Migay <vova.migay123@gmail.com>
2021-07-06 17:55:41 +0300

    Extract to a separate function

--- src/app/game/wait-table/WaitTable.tsx
+++ src/app/game/wait-table/WaitTable.tsx
@@ -4,38 +4,42 @@ import { observer } from 'mobx-react-lite';
 import { v4 } from 'uuid';
 
 import { useCustomTranslation } from '../../hooks';
-import { store } from '../../store';
+import { IGameUser, store } from '../../store';
 import { Header } from './Header';
 import { PlayerCell } from './PlayerCell';
 import { Styled } from './styled';
 
+const renderPlayerCells = (
+  participants: IGameUser[],
+  maxParticipants: number
+) => {
+  const cells: JSX.Element[] = [];
+
+  for (let index = 0; index < maxParticipants; index++) {
+    cells.push(
+      <PlayerCell
+        key={v4()}
+        player={index < participants.length ? participants[index] : undefined}
+      />
+    );
+  }
+
+  return cells;
+};
+
 export const WaitTable = observer(() => {
   const { t } = useCustomTranslation();
 
-  const playerCells = () => {
-    const cells: JSX.Element[] = [];
-
-    for (let index = 0; index < store.room.maxParticipants; index++) {
-      cells.push(
-        <PlayerCell
-          key={v4()}
-          player={
-            index < store.room.participants.length
-              ? store.room.participants[index]
-              : undefined
-          }
-        />
-      );
-    }
-
-    return cells;
-  };
+  const playerCells = renderPlayerCells(
+    store.room.participants,
+    store.room.maxParticipants
+  );
 
   return (
     <Styled.WaitTableContainer>
       <Styled.WaitTable>
         <Header />
-        <Styled.Body>{playerCells()}</Styled.Body>
+        <Styled.Body>{playerCells}</Styled.Body>
         <Styled.Footer>
           {store.room.participants.length !== store.room.maxParticipants && (
             <Styled.StartButton>{t('game.startGame')}</Styled.StartButton>
