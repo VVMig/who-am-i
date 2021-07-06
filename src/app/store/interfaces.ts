d80da82af85a8b0ec068b842fe9743344e7db156
Vladimir Migay <vova.migay123@gmail.com>
2021-07-06 16:25:45 +0300

    Add GameUser interface

--- src/app/store/interfaces.ts
+++ src/app/store/interfaces.ts
@@ -1,5 +1,7 @@
 import { SnapshotIn } from 'mobx-state-tree';
 
+import { GameUser } from './GameUser';
 import { Room } from './Room';
 
 export type IRoomTypes = SnapshotIn<typeof Room>;
+export type IGameUser = SnapshotIn<typeof GameUser>;
