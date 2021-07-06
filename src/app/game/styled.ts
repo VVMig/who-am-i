76c3e42ca3722d08f2ceacb8d5cb0b33a9b918a5
Vladimir Migay <vova.migay123@gmail.com>
2021-07-06 13:21:19 +0300

    Add loading

---
+++ src/app/game/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const GameContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const Styled = {
+  GameContainer,
+};
