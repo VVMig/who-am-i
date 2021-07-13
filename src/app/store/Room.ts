7f277db4e47bc977e96229a77a3eb3de0f858351
Vladimir Migay <vova.migay123@gmail.com>
2021-07-13 16:46:58 +0300

    Add room fields

--- src/app/store/Room.ts
+++ src/app/store/Room.ts
@@ -5,16 +5,27 @@ import { GameUser } from './GameUser';
 
 const defaultMaxParticipants = 0;
 
-export const Room = types.model('Room', {
-  maxParticipants: types.optional(types.number, defaultMaxParticipants),
-  shareId: types.optional(types.string, ''),
-  participants: types.optional(types.array(GameUser), []),
-  gameStage: types.optional(
-    types.enumeration('GameStage', [
-      GameStage.NAME_STAGE,
-      GameStage.PLAY_STAGE,
-      GameStage.WAIT_STAGE,
-    ]),
-    GameStage.WAIT_STAGE
-  ),
-});
+export const Room = types
+  .model('Room', {
+    maxParticipants: types.optional(types.number, defaultMaxParticipants),
+    shareId: types.optional(types.string, ''),
+    participants: types.optional(types.array(GameUser), []),
+    gameStage: types.optional(
+      types.enumeration('GameStage', [
+        GameStage.NAME_STAGE,
+        GameStage.PLAY_STAGE,
+        GameStage.WAIT_STAGE,
+      ]),
+      GameStage.WAIT_STAGE
+    ),
+    nowNaming: types.maybeNull(GameUser),
+    nameSeter: types.maybeNull(GameUser),
+  })
+  .views((self) => ({
+    get isAllHaveGuessName() {
+      return self.participants.reduce(
+        (result, participant) => result && !!participant.guessName,
+        true
+      );
+    },
+  }));
